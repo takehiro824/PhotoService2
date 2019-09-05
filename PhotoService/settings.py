48e1897398aaf3e3d5837e6cfa3464c6ffd5cf28
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-09-05 13:36:53 +0900

    update

--- PhotoService/settings.py
+++ PhotoService/settings.py
@@ -142,11 +142,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/app/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../app/static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
