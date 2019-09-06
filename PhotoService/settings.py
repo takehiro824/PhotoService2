71b2279dd9f7f7510484085b5c23d26482687a68
takehiro824 <51868794+takehiro824@users.noreply.github.com>
2019-09-06 13:08:32 +0900

    edit

--- PhotoService/settings.py
+++ PhotoService/settings.py
@@ -157,3 +157,19 @@ MEDIA_URL = '/media/'
 LOGIN_URL = 'app:login'
 LOGIN_REDIRECT_URL = 'app:index'
 LOGOUT_REDIRECT_URL = 'app:index'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
